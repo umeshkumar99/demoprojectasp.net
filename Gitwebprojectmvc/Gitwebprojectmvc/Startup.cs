31b3d48745247cb7db4965b975374372186a4f1e
umeshkumar99 <36888531+umeshkumar99@users.noreply.github.com>
2018-03-06 21:42:49 +0530

    cretae new solutions for mvc

---
+++ Gitwebprojectmvc/Gitwebprojectmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gitwebprojectmvc.Startup))]
+namespace Gitwebprojectmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
