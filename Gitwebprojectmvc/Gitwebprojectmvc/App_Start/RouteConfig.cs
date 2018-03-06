ed29210d0fb4ac929b09c135c53dce808d98d56c
umeshkumar99 <36888531+umeshkumar99@users.noreply.github.com>
2018-03-06 22:10:36 +0530

    umesh changes2

--- Gitwebprojectmvc/Gitwebprojectmvc/App_Start/RouteConfig.cs
+++ Gitwebprojectmvc/Gitwebprojectmvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Gitwebprojectmvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Customer", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Customer", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
